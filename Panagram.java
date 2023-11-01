611b6466e143a2180f2215837dad62672874a0bb
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2023-11-01 12:45:21 +0530

    Update Panagram.java
    removed collections

--- Panagram.java
+++ Panagram.java
@@ -1,35 +1,37 @@
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
-
 public class Panagram {
-    public static boolean isPangram(String str) {
-        str = str.toLowerCase();
-        Set<Character> alphabetSet = new HashSet<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                alphabetSet.add(ch);
-            }
-        }
-
-        return alphabetSet.size() == 26;
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
         String input = scanner.nextLine();
         scanner.close();
-
-        boolean result = isPangram(input);
-
-        if (result) {
-            System.out.println("Input is a pangram.");
+        boolean isPangram = isPangram(input);
+         if (isPangram) {
+            System.out.println("The input is a pangram.");
         } else {
-            System.out.println("Input is not a pangram.");
+            System.out.println("The input is not a pangram.");
         }
+    }
+     public static boolean isPangram(String str) {
+         boolean[] alphabetPresent = new boolean[26];
+        // Convert the input string to lowercase
+        str = str.toLowerCase();
+       // Iterate through the characters of the string
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+           // Check if the character is a lowercase letter
+            if (ch >= 'a' && ch <= 'z') {
+                // Mark the corresponding letter as present
+                alphabetPresent[ch - 'a'] = true;
+            }
+        }
+
+        for (boolean present : alphabetPresent) {
+            if (!present) {
+                return false; // not pangram
+            }
+        }
+
+        return true; // pangram
     }
 }
